6e4153e952e46c4c2c2e4dc50bb5d56bd41924bb
douglas <douglasfmr1@gmail.com>
2021-12-04 23:34:16 -0300

    ex3 e ex4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const texts = screen.getByText(/No favorite pokemon found/i);
+    expect(texts).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /More details/i });
+
+    userEvent.click(home);
+
+    const pokemao = screen.getByText(/Pokémon favoritado?/i);
+
+    userEvent.click(pokemao);
+
+    history.push('/favorites');
+
+    const texts = screen.getByText(/pikachu/i);
+    expect(texts).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem:', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    const srcImage = ('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(image.src).toBe(srcImage);
+  });
+});
