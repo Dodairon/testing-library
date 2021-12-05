d54749e1f5d6649fa78d0fb9898d12a93b867cec
douglas <douglasfmr1@gmail.com>
2021-12-04 21:09:45 -0300

    ex1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <App.js />', () => {
+  renderWithRouter(<App />);
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const home = screen.getByRole('link', { name: /Home/i });
+    const about = screen.getByRole('link', { name: /About/i });
+    const favPokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favPokemons).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /Home/i });
+
+    userEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de About', () => {
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /about/i });
+
+    userEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const fav = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+    userEvent.click(fav);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/nomecsiste');
+    const nonEcsiste = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji' });
+
+    expect(nonEcsiste).toBeInTheDocument();
+  });
+});
